d1cea7eb92b0ab286a38f3c3c9385ebe21e11482
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-11-16 23:26:17 +0200

    update .eslintrc gulpfile.js package.json dist/index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,34 @@
-const  { task, src, watch, dest } = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const sass = require('gulp-sass');
-const imagemin = require('gulp-imagemin');
+const { task, src, watch, dest } = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const sass = require("gulp-sass");
+const imagemin = require("gulp-imagemin");
 
-sass.compiler = require('node-sass');
+var gulp = require("gulp");
+var ghPages = require("gulp-gh-pages");
+
+gulp.task("deploy", function () {
+  return gulp.src("./dist/**/*").pipe(ghPages());
+});
+
+sass.compiler = require("node-sass");
 
 function scss() {
-  return src('src/scss/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
+  return src("src/scss/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
     .pipe(autoprefixer())
-    .pipe(dest('dist/css'))
+    .pipe(dest("dist/css"));
 }
 
 function imgmin() {
-  return src('src/assets/images/*.*')
-  .pipe(imagemin())
-  .pipe(dest('dist/images'))
+  return src("src/assets/images/*.*")
+    .pipe(imagemin())
+    .pipe(dest("dist/images"));
 }
 
-task('watch', () => {
+task("watch", () => {
   scss();
   imgmin();
 
-  watch('src/assets/images/*.*', imgmin);
-  watch('src/scss/**/*.scss', scss);
+  watch("src/assets/images/*.*", imgmin);
+  watch("src/scss/**/*.scss", scss);
 });
