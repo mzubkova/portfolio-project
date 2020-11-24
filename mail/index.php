6576d6ed758a32ae06dfde403f5c5b2accf7146f
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-11-24 16:11:39 +0200

    Update 2020-11-24T14:11:36.204Z

--- mail/index.php
+++ mail/index.php
@@ -1,19 +1,15 @@
 <!doctype html>
 <html>
-
 <head>
     <meta charset="utf-8" />
-    <title> Создание формы обратной связи </title>
+    <title> Форма обратной связи </title>
     <meta name="description" content="Создание формы обратной связи" />
     <link rel="stylesheet" type="text/css" href="/css/style.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&display=swap&subset=cyrillic" rel="stylesheet">
-
 </head>
-
 <body>
     <div class="form-wrapper">
-
         <form class="contact-form" id="contact-form_1" method="POST">
               <p class="contacts-section__contact-form-text--second contacts-section__contact-form-text">Оставьте заявку и я свяжусь с вами, чтобы уточнить детали фотосессии</p>
               <p class="contact-form__description"></p>
@@ -30,13 +26,12 @@
                   <div class="contact-form__error contact-form__error-email"></div>
                 </div>
                 <div class="contact-form__input-wrapper">
-                  <textarea name="message" id="textarea" cols="30" rows="7" placeholder="Сообщение"></textarea>
+                  <textarea name="message" id="textarea" cols="20" rows="5" placeholder="Сообщение"></textarea>
                 </div>
-                <button type="submit" class="contact-form__button btn">Отправить</button>
-              </form> -->
+                <button type="submit" class="contact-form__button btn-intro">Отправить</button>
+              </form>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="/mail/js/mail.js"></script>
 </body>
-
 </html>
