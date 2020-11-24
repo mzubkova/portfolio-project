29dae2258dba99877b1b59210390f1acf8e3fe0c
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-11-24 17:39:00 +0200

    Update 2020-11-24T15:38:58.029Z

--- sendform.php
+++ sendform.php
@@ -5,14 +5,14 @@ $email = trim($_POST['email']);
 $message = trim($_POST['message']);
 $fromMail = '_mainaccount@kvkstudio.com.ua'; //Почта отправителя (в домене почты должен быть адрес вашего сайта)
 $fromName = 'Поступила заявка с сайта'; //Заголовок письма
-$emailTo = 'violamars777@gmail.com'; //Ваша почта kvk.creative.studio@gmail.com
+$emailTo = 'kvk.creative.studio@gmail.com'; //Ваша почта
 $subject = 'Форма обратной связи kvkstudio.com.ua';
 $subject = '=?utf-8?b?'. base64_encode($subject) .'?=';
 $headers = "Content-type: text/plain; charset=\"utf-8\"\r\n";
 $headers .= "From: ". $fromName ." <". $fromMail ."> \r\n";
 
 // Содержимое письма
-$body = "Получено письмо с сайта kvkstudio.com.ua \nИмя: $name\nТелефон: $phone\n\Почта: $email \n\Сообщение: $message";
+$body = "Получено письмо с сайта kvkstudio.com.ua \nИмя: $name\nТелефон: $phone\nПочта: $email \nСообщение: $message";
 
 // сообщение будет отправлено в случае, если поле с номером телефона не пустое
 if (strlen($phone) > 0) {
