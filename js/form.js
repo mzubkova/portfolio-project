0bf187ceafd12ae258f7a2655b264fb87d1f5660
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-11-25 15:56:16 +0200

    Update 2020-11-25T13:56:12.789Z

--- js/form.js
+++ js/form.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-	$("form").submit(function() { // Событие отправки с формы
-		var form_data = $(this).serialize(); // Собираем данные из полей
-		$.ajax({
-			type: "POST", // Метод отправки
-			url: "sendform.php", // Путь к PHP обработчику sendform.php
-			data: form_data,
-			success: function() {
-				$('.popup').addClass('active');
-				$('.thanks').addClass('active');
-			}
-		});
-		event.preventDefault();
-	});
+$(document).ready(function () {
+  $("form").submit(function () {
+    var form_data = $(this).serialize();
+    $.ajax({
+      type: "POST",
+      url: "sendform.php",
+      data: form_data,
+      success: function () {
+        $(".popup").addClass("active");
+        $(".thanks").addClass("active");
+      },
+    });
+    event.preventDefault();
+  });
 });
