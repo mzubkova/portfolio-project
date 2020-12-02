13dcf78ee2e25bc77a25f8f21633af8fa28f6651
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-12-02 18:30:19 +0200

    update styles

--- gulpfile.js
+++ gulpfile.js
@@ -32,18 +32,3 @@ var ghPages = require("gulp-gh-pages");
 gulp.task("deploy", function () {
   return gulp.src("./dist/**/*").pipe(ghPages());
 });
-
-var purify = require("gulp-purifycss");
-
-gulp.task("css", function () {
-  return gulp
-    .src("./public/app/example.css")
-    .pipe(purify(["./public/app/**/*.js", "./public/**/*.html"]))
-    .pipe(gulp.dest("./dist/"));
-});
-
-var postcss = require("gulp-postcss");
-
-gulp.task("css", function () {
-  return gulp.src("./src/*.css").pipe(postcss()).pipe(gulp.dest("./dest"));
-});
