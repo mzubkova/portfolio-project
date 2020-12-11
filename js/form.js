7dd2d5b16926e2a5a40c30b7132836f6f8da1a99
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-12-11 17:45:21 +0200

    Update 2020-12-11T15:45:17.689Z

--- js/form.js
+++ js/form.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
+  $("#ModalLabel").validate({
+    errorElement: "div",
+    errorClass: "contact-form__error-name",
+    rules: {
+      name: {
+        required: true,
+        minlength: 5,
+      },
+      tel: {
+        required: true,
+        minlength: 10,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {
+      name: {
+        required: "Это поле необходимо заполнить!",
+        minlength: jQuery.validator.format(
+          "Длина имени должна быть больше 5-ти символов"
+        ),
+      },
+      tel: {
+        required: "Некорректно введен номер телефона!",
+      },
+      email: {
+        required: "Некорректно введен e-mail!",
+      },
+    },
+    errorPlacement: function (error, element) {
+      $(element).parent().next("div").html(error);
+    },
+  });
   $("form").submit(function () {
     event.preventDefault();
     var form_data = $(this).serialize();
