a440ed96b2bcaf0ee36069a9987e9080e4fd7529
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-12-13 13:06:26 +0200

    Update 2020-12-13T11:06:21.640Z

--- js/form.js
+++ js/form.js
@@ -1,47 +1,15 @@
 $(document).ready(function () {
-  $("#ModalLabel").validate({
-    errorElement: "div",
-    errorClass: "contact-form__error-name",
-    rules: {
-      name: {
-        required: true,
-        minlength: 5,
+  $("form").submit(function () {
+    var form_data = $(this).serialize();
+    $.ajax({
+      type: "POST",
+      url: "sendform.php",
+      data: form_data,
+      success: function () {
+        $(".popup").addClass("active");
       },
-      tel: {
-        required: true,
-        minlength: 10,
-      },
-      email: {
-        required: true,
-        email: true,
-      },
-    },
-    messages: {
-      name: {
-        required: "Это поле необходимо заполнить!",
-      },
-      tel: {
-        required: "Некорректно введен номер телефона!",
-      },
-      email: {
-        required: "Некорректно введен e-mail!",
-      },
-    },
-    errorPlacement: function (error, element) {
-      $(element).parent().next("div").html(error);
-    },
-    submitHandler: function (form) {
-      event.preventDefault();
-      var form_data = $(this).serialize();
-      $.ajax({
-        type: "POST",
-        url: "sendform.php",
-        data: form_data,
-        success: function () {
-          $(".popup").addClass("active");
-        },
-      });
-    },
+    });
+    event.preventDefault();
   });
 });
 
