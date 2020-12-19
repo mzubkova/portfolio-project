fb97030b613c7b92646a4864e8802b04a068cc8b
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-12-19 14:17:09 +0200

    Update 2020-12-19T12:17:05.366Z

--- js/form.js
+++ js/form.js
@@ -1,23 +1,3 @@
-// $(document).ready(function () {
-//   $("form").submit(function () {
-//     var form_data = $(this).serialize();
-//     $.ajax({
-//       type: "POST",
-//       url: "sendform.php",
-//       data: form_data,
-//       success: function () {
-//         $(".popup").addClass("active");
-//       },
-//     });
-//     event.preventDefault();
-//   });
-// });
-
-// $(".close, .popup__button").click(function () {
-//   $(".popup").removeClass("active");
-//   $("form").trigger("reset");
-// });
-
 $(document).ready(function () {
   $("#ModalLabel").validate({
     ignore: ":hidden",
@@ -60,13 +40,65 @@ $(document).ready(function () {
         url: "sendform.php",
         data: $(form).serialize(),
         success: function () {
-          $(".close, .popup__button").click(function () {
-            $(".popup").removeClass("active");
-            $("form").trigger("reset");
-          });
+          $(".popup").addClass("active");
         },
       });
       return false;
     },
   });
+});
+
+$(document).ready(function () {
+  $("#formOnPage").validate({
+    ignore: ":hidden",
+    errorElement: "div",
+    errorClass: "contact-form__error-name",
+    rules: {
+      name: {
+        required: true,
+        minlength: 5,
+      },
+      tel: {
+        required: true,
+        minlength: 10,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {
+      name: {
+        required: "Это поле необходимо заполнить!",
+        minlength: jQuery.validator.format(
+          "Длина имени должна быть больше 5-ти символов"
+        ),
+      },
+      tel: {
+        required: "Некорректно введен номер телефона!",
+      },
+      email: {
+        required: "Некорректно введен e-mail!",
+      },
+    },
+    errorPlacement: function (error, element) {
+      $(element).parent().next("div").html(error);
+    },
+    submitHandler: function (form) {
+      $.ajax({
+        type: "POST",
+        url: "sendform.php",
+        data: $(form).serialize(),
+        success: function () {
+          $(".popup").addClass("active");
+        },
+      });
+      return false;
+    },
+  });
+});
+
+$(".close, .popup__button").click(function () {
+  $(".popup").removeClass("active");
+  $("form").trigger("reset");
 });
