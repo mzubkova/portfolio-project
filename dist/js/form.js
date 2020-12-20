7995b1bac40c6bf789f615f068c445351dd16cd9
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-12-20 13:43:06 +0200

    update adaptive

--- dist/js/form.js
+++ dist/js/form.js
@@ -1,19 +1,104 @@
 $(document).ready(function () {
-  $("form").submit(function () {
-    event.preventDefault();
-    var form_data = $(this).serialize();
-    $.ajax({
-      type: "POST",
-      url: "sendform.php",
-      data: form_data,
-      success: function () {
-        $(".popup").addClass("active");
+  $("#ModalLabel").validate({
+    ignore: ":hidden",
+    errorElement: "div",
+    errorClass: "contact-form__error-name",
+    rules: {
+      name: {
+        required: true,
+        minlength: 5,
       },
-    });
+      tel: {
+        required: true,
+        minlength: 10,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {
+      name: {
+        required: "Это поле необходимо заполнить!",
+        minlength: jQuery.validator.format(
+          "Длина имени должна быть больше 5-ти символов"
+        ),
+      },
+      tel: {
+        required: "Некорректно введен номер телефона!",
+      },
+      email: {
+        required: "Некорректно введен e-mail!",
+      },
+    },
+    errorPlacement: function (error, element) {
+      $(element).parent().next("div").html(error);
+    },
+    submitHandler: function (form) {
+      $.ajax({
+        type: "POST",
+        url: "sendform.php",
+        data: $(form).serialize(),
+        success: function () {
+          $(".popup").addClass("active");
+        },
+      });
+      return false;
+    },
   });
 });
 
-$(".close, .btn-modal").click(function () {
+$(document).ready(function () {
+  $("#formOnPage").validate({
+    ignore: ":hidden",
+    errorElement: "div",
+    errorClass: "contact-form__error-name",
+    rules: {
+      name: {
+        required: true,
+        minlength: 5,
+      },
+      tel: {
+        required: true,
+        minlength: 10,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {
+      name: {
+        required: "Это поле необходимо заполнить!",
+        minlength: jQuery.validator.format(
+          "Длина имени должна быть больше 5-ти символов"
+        ),
+      },
+      tel: {
+        required: "Некорректно введен номер телефона!",
+      },
+      email: {
+        required: "Некорректно введен e-mail!",
+      },
+    },
+    errorPlacement: function (error, element) {
+      $(element).parent().next("div").html(error);
+    },
+    submitHandler: function (form) {
+      $.ajax({
+        type: "POST",
+        url: "sendform.php",
+        data: $(form).serialize(),
+        success: function () {
+          $(".popup").addClass("active");
+        },
+      });
+      return false;
+    },
+  });
+});
+
+$(".close, .popup__button").click(function () {
   $(".popup").removeClass("active");
   $("form").trigger("reset");
 });
