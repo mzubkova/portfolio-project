bc4dfb301d518d7822501d512625b32ddbac6f1e
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-12-29 00:43:55 +0200

    Update 2020-12-28T22:43:51.343Z

--- js/modal.js
+++ js/modal.js
@@ -71,3 +71,12 @@ document.addEventListener("DOMContentLoaded", function () {
     this.classList.remove("active");
   });
 });
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
